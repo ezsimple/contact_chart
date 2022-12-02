85316b65e780c6bf427428db259c9c23e2d95e26
mhlee <mkeasy@gmail.com>
2022-12-02 13:32:46 +0900

    change link to clone

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SRC=/home/mkeasy/study/my/02.python/contact_anal/chart
 DST=src/static/media
-# \cp -f $SRC/* $DST
-ln -s $SRC/* $DST
+\cp -f $SRC/* $DST
+# ln -s $SRC/* $DST # 링크된 데이터는 github에 '경로명'이 전달된다.
