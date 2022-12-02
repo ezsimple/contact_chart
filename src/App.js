d31af5f6407625bb6b4d3b43963e94d96d971a22
mhlee <mkeasy@gmail.com>
2022-12-02 10:36:47 +0900

    porting from flask to react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { readString } from 'react-papaparse';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -13,71 +15,80 @@ import fname6 from 'static/images/전체 입사자수 .vs. 퇴사자수.png';
 import fname9 from 'static/images/전체 입사율, 퇴사율.png';
 import fname8 from 'static/images/인력 변경율, 채용 성공율.png';
 
-function App() {
-  const period = '21/11 ~ 22/11';
+import periodCsv from 'static/images/period.txt';
 
-  const fetchPeriod = () => {
-    const periodCsv = 'static/images/period.csv';
-    return fetch(periodCsv).then(function (res) {
-      const reader = res.body.getReader();
-      const decoder = new TextDecoder('utf-8');
-      return reader.read().then(function (result) {
-        return decoder.decode(result.value);
-      });
-    });
+function App() {
+  const [period, setPeriod] = useState('');
+
+  const papaConfig = {
+    complete: (results, file) => {
+      const { data } = results;
+      setPeriod(data[1][1]);
+    },
+    download: true,
+    error: (error, file) => {},
   };
 
   useEffect(() => {
-    const data = fetchPeriod();
-    console.log(data);
-  }, []);
+    readString(periodCsv, papaConfig);
+  }, [readString]);
 
   return (
-    <div class="flex justify-center">
-      <header class="masthead mb-auto">
-        <div class="inner">
-          <h3>Recruiting Statistics</h3>
-          <div class="text-center">
-            <h4>({period})</h4>
+    <div className="container">
+      <div className="row">
+        <header>
+          <div className="text-center">
+            <h2>Recruiting Statistics</h2>
+            <div>
+              <h4>({period})</h4>
+            </div>
+          </div>
+        </header>
+      </div>
+
+      <div className="row">
+        <div className="text-center">
+          <div>
+            <img src={fname0} alt="월별 입사자수" />
+          </div>
+          <div>
+            <img src={fname1} alt="월별 퇴사자수" />
+          </div>
+          <div>
+            <img src={fname2} alt="월별 고용 성장율" />
+          </div>
+          <div>
+            <img src={fname3} alt="부서별 입사자수 .vs. 퇴사자수" />
+          </div>
+          <div>
+            <img src={fname4} alt="직급별 입사자수,퇴사자수" />
+          </div>
+          <div>
+            <img src={fname5} alt="부서별 바로 퇴사자수" />
+          </div>
+          <div>
+            <img src={fname7} alt="직급별 바로 퇴사자수" />
+          </div>
+          <div>
+            <img src={fname6} alt="전체 입사자수 .vs. 퇴사자수" />
+          </div>
+          <div>
+            <img src={fname9} alt="전체 입사율, 퇴사율" />
+          </div>
+          <div>
+            <img src={fname8} alt="인력 변경율, 채용 성공율" />
           </div>
         </div>
-      </header>
-      <div class="outline w-25 pa3 mr2">
-        <img src={fname0} alt="월별 입사자수" />
       </div>
-      <div class="outline w-25 pa3 mr2">
-        <img src={fname1} alt="월별 퇴사자수" />
+
+      <div className="row">
+        <footer className="mastfoot mt-auto">
+          <div className="inner text-center">
+            <p>made by mhlee</p>
+            <p>월별 직원연락망 분석용, 시각화 자료입니다</p>
+          </div>
+        </footer>
       </div>
-      <div class="outline w-25 pa3 mr2">
-        <img src={fname2} alt="월별 고용 성장율" />
-      </div>
-      <div class="outline w-25 pa3 mr2">
-        <img src={fname3} alt="부서별 입사자수 .vs. 퇴사자수" />
-      </div>
-      <div class="outline w-25 pa3">
-        <img src={fname4} alt="직급별 입사자수,퇴사자수" />
-      </div>
-      <div class="outline w-25 pa3">
-        <img src={fname5} alt="부서별 바로 퇴사자수" />
-      </div>
-      <div class="outline w-25 pa3">
-        <img src={fname7} alt="직급별 바로 퇴사자수" />
-      </div>
-      <div class="outline w-25 pa3">
-        <img src={fname6} alt="전체 입사자수 .vs. 퇴사자수" />
-      </div>
-      <div class="outline w-25 pa3">
-        <img src={fname9} alt="전체 입사율, 퇴사율" />
-      </div>
-      <div class="outline w-25 pa3">
-        <img src={fname8} alt="인력 변경율, 채용 성공율" />
-      </div>
-      <footer class="mastfoot mt-auto">
-        <div class="inner">
-          <p>made by mhlee</p>
-          <p>월별 직원연락망 분석용, 시각화 자료입니다</p>
-        </div>
-      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
